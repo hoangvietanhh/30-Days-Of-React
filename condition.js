fe34c2524aaea64c20640080717118db0a3a01db
hoangvietanh <hvanh@cmc.com.vn>
2021-11-23 17:40:18 +0700

    commit

---
+++ condition.js
@@ -0,0 +1,12 @@
+let age = prompt('Enter your age: ')
+
+switch (age) {
+  case age == 30:
+    alert('You are old enough to drive.')
+    break;
+  case age == 15:
+    alert('You are left with 3 years to drive.')
+    break;
+  default:
+    alert('Please don\'t')
+}
